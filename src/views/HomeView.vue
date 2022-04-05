acdf85b5e55357f08274647db5f82f33d02de686
DeeGeeWest <DeeGeeWest@gmail.com>
2022-04-04 22:30:11 -0400

    everything simple min capstone code

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,77 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>{{ message }}</h1>
+    <h2>New Product</h2>
+    Name
+    <input type="text" v-model="productName" />
+    Description
+    <input type="text" v-model="productDescription" />
+    Price
+    <input type="text" v-model="productPrice" />
+    Image URL
+    <input type="text" v-model="productImage_Url" />
+    <button v-on:click="createProduct">Create</button>
+    <div v-for="product in products" v-bind:key="product.id">
+      <h3>{{ product.name }}</h3>
+      <h4>Description: {{ product.description }}</h4>
+      <p>price: {{ product.price }}</p>
+      <img v-bind:src="product.image_url" v-bind:alt="product.name" />
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      message: "simple mini capstone",
+      message2: "bleh",
+      greeting: "ayo rocko!!",
+      products: [],
+      productName: "",
+      productDescription: "",
+      productPrice: 0,
+      productImage_Url: "",
+    };
+  },
+  created: function () {
+    this.indexProducts();
+  },
+  methods: {
+    indexProducts() {
+      axios.get("http://localhost:3000/products").then((response) => {
+        console.log(response.data);
+        this.products = response.data;
+      });
+    },
+    createProduct() {
+      var params = {
+        name: this.productName,
+        description: this.productDescription,
+        price: this.productPrice,
+        image_url: this.productImage_Url,
+      };
+      axios
+        .post("http://localhost:3000/products", params)
+        .then((response) => {
+          this.products.push(response.data);
+        })
+        .catch((error) => {
+          console.log(error.response);
+        });
+    },
   },
 };
 </script>
+
+<style>
+h4 {
+  color: hotpink;
+  font: 600;
+}
+h3 {
+  color: green;
+}
+</style>
