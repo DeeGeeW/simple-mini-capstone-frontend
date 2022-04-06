5ecbae8878b698fe4be5045fe1d79c33b1f05af0
DeeGeeWest <DeeGeeWest@gmail.com>
2022-04-05 21:54:44 -0400

    add delete show and update functions

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,54 @@
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
+    <h1>{{ message }} Count: {{ products.length }}</h1>
     <h2>New Product</h2>
     Name
-    <input type="text" v-model="productName" />
+    <input type="text" v-model="newProduct.name" />
     Description
-    <input type="text" v-model="productDescription" />
+    <input type="text" v-model="newProduct.description" />
     Price
-    <input type="text" v-model="productPrice" />
+    <input type="text" v-model="newProduct.price" />
     Image URL
-    <input type="text" v-model="productImage_Url" />
+    <input type="text" v-model="newProduct.image_url" />
     <button v-on:click="createProduct">Create</button>
     <div v-for="product in products" v-bind:key="product.id">
       <h3>{{ product.name }}</h3>
+      <div>
+        <button v-on:click="showProduct(product)">Info</button>
+      </div>
       <h4>Description: {{ product.description }}</h4>
       <p>price: {{ product.price }}</p>
-      <img v-bind:src="product.image_url" v-bind:alt="product.name" />
+      <img v-bind:src="product.image_url" v-bind:alt="product.name" style="max-width: 250px" />
     </div>
+    <dialog id="product-details">
+      <form method="dialog">
+        <h1>Product Info</h1>
+        <p>Name: {{ currentProduct.name }}</p>
+        <p>Description: {{ currentProduct.description }}</p>
+        <p>Price: {{ currentProduct.price }}</p>
+        <p>Image_url: {{ currentProduct.image_url }}</p>
+        <h1>Update Product</h1>
+        <p>
+          Name:
+          <input v-model="currentProduct.name" type="text" />
+        </p>
+        <p>
+          Description:
+          <input v-model="currentProduct.description" type="text" />
+        </p>
+        <p>
+          Price:
+          <input v-model="currentProduct.price" type="text" />
+        </p>
+        <p>
+          Image_url:
+          <input v-model="currentProduct.image_url" type="text" />
+        </p>
+        <button v-on:click="updateProduct()">Update</button>
+        <button v-on:click="deleteProduct(currentProduct)">Delete</button>
+        <button>Close</button>
+      </form>
+    </dialog>
   </div>
 </template>
 
@@ -26,14 +58,10 @@ import axios from "axios";
 export default {
   data: function () {
     return {
-      message: "simple mini capstone",
-      message2: "bleh",
-      greeting: "ayo rocko!!",
+      message: "Simple Mini Capstone:",
       products: [],
-      productName: "",
-      productDescription: "",
-      productPrice: 0,
-      productImage_Url: "",
+      newProduct: {},
+      currentProduct: {},
     };
   },
   created: function () {
@@ -47,16 +75,42 @@ export default {
       });
     },
     createProduct() {
-      var params = {
-        name: this.productName,
-        description: this.productDescription,
-        price: this.productPrice,
-        image_url: this.productImage_Url,
-      };
       axios
-        .post("http://localhost:3000/products", params)
+        .post("http://localhost:3000/products", this.newProduct)
         .then((response) => {
           this.products.push(response.data);
+        })
+        .catch((error) => {
+          console.log(error.response);
+        });
+    },
+    showProduct(product) {
+      axios.get(`http://localhost:3000/products/${product.id}`).then((response) => {
+        this.currentProduct = response.data;
+        this.editProduct = response.data;
+        document.querySelector("#product-details").showModal();
+      });
+    },
+    updateProduct() {
+      axios
+        .patch(`http://localhost:3000/products/${this.currentProduct.id}`, this.currentProduct)
+        .then((response) => {
+          console.log(response);
+          var index = this.products.indexOf(this.currentProduct);
+          this.products.splice(index, 1);
+          this.products.push(response.data);
+        })
+        .catch((error) => {
+          console.log(error.response);
+        });
+    },
+    deleteProduct(product) {
+      axios
+        .delete(`http://localhost:3000/products/${product.id}`)
+        .then((response) => {
+          console.log(response);
+          var index = this.products.indexOf(product);
+          this.products.splice(index, 1);
         })
         .catch((error) => {
           console.log(error.response);
@@ -73,5 +127,8 @@ h4 {
 }
 h3 {
   color: green;
+}
+h1 {
+  color: blue;
 }
 </style>
